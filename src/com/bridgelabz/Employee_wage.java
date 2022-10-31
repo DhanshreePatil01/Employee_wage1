99416e1a2e757a083264c038d21e7bdde169cc2d
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-10-31 12:35:17 +0530

    Added :UC3 added part time employee and wage.

--- src/com/bridgelabz/Employee_wage.java
+++ src/com/bridgelabz/Employee_wage.java
@@ -4,6 +4,7 @@ public class Employee_wage {
 
 	public static void main(String[] args) {
 		
+		int IS_PART_TIME = 1; 
 		 int IS_FULL_TIME = 1; 
 		 int EMP_RATE_PER_HOUR = 20; //constant
 		 
@@ -11,15 +12,17 @@ public class Employee_wage {
 		 int empWage = 0;            //variables
 
 		 // computation
-		 double emp_check=Math.floor(Math.random()*10)%2;
+		 double emp_check=Math.floor(Math.random()*10)%3;
 		 
-		 if(emp_check==IS_FULL_TIME)
+		 if(emp_check==IS_PART_TIME)
 			 
-			 empHrs = 8 ;
+			 empHrs = 4 ;
 		 
+		 else if(emp_check==IS_FULL_TIME)
+			 
+			 empHrs =8;
 		 else
-			 
-			 empHrs =0;
+			 empHrs=0;
 		 
 		 empWage = empHrs * EMP_RATE_PER_HOUR;
 		 System.out.println("emp wage: "+empWage);
