845543f704f60d33e8b8c4b43e16f5e624edd162
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-11-01 00:11:15 +0530

    Added :UC4 Employee Wage Calculation Using Switch.

--- src/com/bridgelabz/Employee_wage.java
+++ src/com/bridgelabz/Employee_wage.java
@@ -2,27 +2,33 @@ package com.bridgelabz;
 
 public class Employee_wage {
 
+	public static final int IS_PART_TIME = 1; 
+	public static final int IS_FULL_TIME = 2; 
+	public static final int EMP_RATE_PER_HOUR = 20; //constant
+	
 	public static void main(String[] args) {
 		
-		int IS_PART_TIME = 1; 
-		 int IS_FULL_TIME = 1; 
-		 int EMP_RATE_PER_HOUR = 20; //constant
-		 
+		
 		 int empHrs = 0;
 		 int empWage = 0;            //variables
 
 		 // computation
-		 double emp_check=Math.floor(Math.random()*10)%3;
+		 int emp_check=(int)Math.floor(Math.random()*10)%3;
 		 
-		 if(emp_check==IS_PART_TIME)
+		 switch(emp_check)
+		 {
+		 case IS_PART_TIME :
+			 empHrs=4;
+			 break;
 			 
-			 empHrs = 4 ;
-		 
-		 else if(emp_check==IS_FULL_TIME)
+		 case IS_FULL_TIME:
+			 empHrs=8;
+			 break;
 			 
-			 empHrs =8;
-		 else
+	     default:
 			 empHrs=0;
+			 
+		 }
 		 
 		 empWage = empHrs * EMP_RATE_PER_HOUR;
 		 System.out.println("emp wage: "+empWage);
