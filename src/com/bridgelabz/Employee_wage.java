7dacb280dbca8255d0d045f449fdb46f8098acb4
DhanshreePatil01 <patildhanshrees6112@gmail.com>
2022-10-31 10:50:09 +0530

    Added :UC2 Employee Daily Wage Calculation.

--- src/com/bridgelabz/Employee_wage.java
+++ src/com/bridgelabz/Employee_wage.java
@@ -4,18 +4,25 @@ public class Employee_wage {
 
 	public static void main(String[] args) {
 		
-		int IS_FULL_TIME = 1; 
+		 int IS_FULL_TIME = 1; 
+		 int EMP_RATE_PER_HOUR = 20; //constant
 		 
-       double emp_check=Math.floor(Math.random()*10)%2;
+		 int empHrs = 0;
+		 int empWage = 0;            //variables
+
+		 // computation
+		 double emp_check=Math.floor(Math.random()*10)%2;
 		 
 		 if(emp_check==IS_FULL_TIME)
 			 
-			 System.out.println("Employee is present"); 
+			 empHrs = 8 ;
 		 
 		 else
 			 
-			 System.out.println("Employee is Abscent"); 
-
+			 empHrs =0;
+		 
+		 empWage = empHrs * EMP_RATE_PER_HOUR;
+		 System.out.println("emp wage: "+empWage);
 	}
 
 }
